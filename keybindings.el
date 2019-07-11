3a2cf163fe45a8fc5c03933885a36e333763e879
Niels uit de Bos <nielius@gmail.com>
2019-07-11 11:31:57 +0200

    Reorganize keybindings; massive reorganization
    - Remove unnecessary code and comments.
    - Rearrange.
    - Add some better comments.

--- keybindings.el
+++ keybindings.el
@@ -1,19 +1,10 @@
-;; ** Keybindings voor eigen zoekfuncties
-;; todo: <f2> i : zoek met helm-swoop het index-bestand van mijn aantekeningen
-;; (define-key global-map (kbd "<f2> s") 'nielius-grep-org-files)
-;; (define-key global-map (kbd "<f2> d") 'nielius-grep-studie-files)
-;; (define-key global-map (kbd "<f2> f") (lambda () (interactive) (helm-find-1 "~/Dropbox/")))
-;; (define-key global-map (kbd "<f2> b") (lambda () (interactive) (helm-find-1 "~/md/"))) ; wiskundeboeken
-
 (require 'helm-find) ;; is this executed? otherwise, I need to put this in a defun that replaces the commands for of, op, ob (or use progn?)
 
-;; Override some general keybindings
+;; Override some general key bindings
 (define-key evil-normal-state-map (kbd "gf") 'nielius-ffap)
-(define-key evil-normal-state-map (kbd "<up>") 'm/scrollup)
-(define-key evil-normal-state-map (kbd "<down>") 'm/scrolldown)
+(define-key evil-normal-state-map (kbd "<mouse-2>") 'eval-last-sexp)
 
-(define-key evil-normal-state-map (kbd "<mouse-2>") 'my-eval-curr-sexp)
-
+;; Global key bindings for evaluating emacs lisp in any file type.
 (spacemacs/set-leader-keys
   "er" 'eval-region
   "ec" 'nielius-eval-curr-sexp
@@ -21,9 +12,14 @@
   )
 
 
-;; Keybindings for eyebrowse-switch-to-window-config-n with n an integer
+;; Global key bindings
+(evil-global-set-key 'normal (kbd "~") 'evil-my-paste-at-mark)
+(define-key evil-insert-state-map (kbd "M-p") 'yas-expand)
+
+
+;; Key bindings for eyebrowse-switch-to-window-config-n with n an integer
 ;;
-;; For every 1 \leq n \leq 9, map SPC n to switch to eyebrowse window configuration n
+;; For every 1 <= n <= 9, map SPC n to switch to eyebrowse window configuration n
 (mapc
  (lambda (n)
    (spacemacs/set-leader-keys
@@ -34,42 +30,20 @@
  (number-sequence 1 9))
 
 
-;; Keybindings for finding files
+;; Generally useful global shortcuts available under `SPC o'
 (spacemacs/set-leader-keys
-  ; "os" 'nielius-grep-studie-files ;; gebruikte ik niet echt
-  "os" 'create-link-to-desktop
-  "od" 'nielius-grep-org-files
-  "of" (lambda () (interactive) (helm-find-1 "~/doc/"))
-  "op" (lambda () (interactive) (helm-find-1 "~/.spacemacs.d/private/")) ; configure private layers
-  "ob" (lambda () (interactive) (helm-find-1 "~/md/"))
-  "oB" (lambda () (interactive) (helm-find-1 "~/library/"))
-  "oe" 'nielius-sh-execute-region-or-line ; send current line or region to inferior shell; 'e' for execute; compare to the global "SPC e e" for evaluation sexps anywhere; see also nielius-xargs-on-region
-  "oH" (lambda () (interactive) (find-file "~/doc/org/home.org"))
-  "ol" (lambda () (interactive) (ace-link)
-         (spacemacs/toggle-maximize-buffer)) ; open link and immediately maximize
-  ; "wn" 'make-frame ; dit is nu SPC w F
-  "oo" 'nielius-xargs-xdg-open-smart
-  "oy" 'buffer-file-name-to-kill-ring
-  ;; Relativize the filename at point is useful in combination with buffer-file-name-to-kill-ring.
-  "or" (lambda () (interactive (find-file "~/doc/notes/agenda/.latest_docs.md")))
-  "ox" 'nielius-xargs-on-region
+  "os" 'create-link-to-desktop ; `s' for symlink
+  "oy" 'buffer-file-name-to-kill-ring ; better: SPC f y y  (spacemacs/copy-file-path)
+  ;; Key bindings to execute pieces of shell code or to open files. Compare to
+  ;; `SPC e e' for evaluation of sexps anywhere.
+  "oe" 'nielius-sh-execute-region-or-line ; `e' for execute
+  "oo" 'nielius-xargs-xdg-open-smart ; `o' for open
+  "ox" 'nielius-xargs-on-region ; `x' for xargs
   )
 
-(spacemacs/set-leader-keys-for-major-mode 'org-mode ; works only in org-mode, using SPC m or ,
-  "io" 'helm-okular ; insert org link; eigenlijk deel van een andere layer?
-  "iO" 'okular-dbus-insert-default-link ; komt ook van andere layer
-  "oh" 'open-the-exported-html
-  "oq" 'org-latex-export-to-pdf-cd) ; q voor quick export
 
-
-(define-key evil-insert-state-map (kbd "M-p") 'yas-expand)
-
-;; Keybindings for markdown mode
+;; Key bindings for markdown mode
 (spacemacs/set-leader-keys-for-major-mode 'markdown-mode "v" 'open-this-file-in-browser)
-
-
-;; Global keybindings
-(evil-global-set-key 'normal (kbd "~") 'evil-my-paste-at-mark)
 
 
 ;; Reftex-keybindings
@@ -81,7 +55,3 @@
   '(progn
      (define-key reftex-toc-mode-map  "j" 'reftex-toc-next)
      (define-key reftex-toc-mode-map "k" 'reftex-toc-previous)))
-
-;; Maar dit is eigenlijk niet helemaal wat ik wil.
-;; Ik wil gewoon in evil-mode zitten en een paar handige dingen van reftex.
-;; Misschien als inspiratie hoe dired werkt?
